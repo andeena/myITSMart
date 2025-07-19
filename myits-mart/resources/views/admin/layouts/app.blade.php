62642f8e9bb5b9bb1c6565c66650fd8bd8de1bf1
andeena <andinabks@gmail.com>
2025-07-20 03:22:53 +0700

    solve

--- myits-mart/resources/views/admin/layouts/app.blade.php
+++ myits-mart/resources/views/admin/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="id">
 <head>
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title') - Admin Panel myITS Mart</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">

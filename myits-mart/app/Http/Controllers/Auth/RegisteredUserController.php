cb784b40e2af40eee020f648de8e749cbe880ce8
andeena <andinabks@gmail.com>
2025-07-20 00:45:40 +0700

    regist akun

--- myits-mart/app/Http/Controllers/Auth/RegisteredUserController.php
+++ myits-mart/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Profile;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -39,6 +40,10 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+        ]);
+
+        Profile::create([
+            'user_id' => $user->id,
         ]);
 
         event(new Registered($user));

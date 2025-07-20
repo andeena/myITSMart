43983c8ad245a33c619de4957272b28c72137f84
andeena <andinabks@gmail.com>
2025-07-20 16:55:23 +0700

    config

--- myits-mart/vite.config.js
+++ myits-mart/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: 'https://myitsmart-production-00f8.up.railway.app/',
+    base: 'http://myitsmart-production-00f8.up.railway.app/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
